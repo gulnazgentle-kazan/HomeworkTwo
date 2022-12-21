0aa69677107ae6fe1cf64562204eb3932e7b9df4
gulnazgentle-kazan <gulnazvp@mail.ru>
2022-12-21 16:56:17 +0300

    did and added exersise 13

---
+++ ExerciseThirteen/Program.cs
@@ -0,0 +1,54 @@
+﻿/*Задача 13: Напишите программу, 
+которая выводит третью цифру заданного числа 
+или сообщает, что третьей цифры нет.
+
+645 -> 5
+
+78 -> третьей цифры нет
+
+32679 -> 6*/
+
+Console.WriteLine("Введите любое целое число");
+int number = Convert.ToInt32(Console.ReadLine());
+// Для положительных чисел
+if (number < 100 && number > 0)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+if (number > 99 && number < 1000)
+{
+    int result = (number % 10);
+    Console.WriteLine($"Третья цифра равна {result}");
+}
+if (number > 999)
+{
+    while (number > 999)
+    {
+        number = number / 10;
+    }
+    Console.WriteLine($"Третья цифра равна {number % 10}");
+}
+
+//Для отрицательных чисел
+if (number > -100 && number < 0)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+if (number < -99 && number > -1000)
+{
+    int result = (number % 10);
+    Console.WriteLine($"Третья цифра равна {-(result)}");
+}
+if (number < -999)
+{
+    while (number < -999)
+    {
+        number = number / 10;
+    }
+    Console.WriteLine($"Третья цифра равна {-(number % 10)}");
+}
+
+
+
+
+
